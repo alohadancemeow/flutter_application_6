e2dec873682ecf940c16dc825cd0ca110902e3ad
Alohadancemeow <nawongsak@gmail.com>
2024-02-16 15:00:30 +0700

    ✅ set up pages

---
+++ lib/constant.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+// bg colors
+var backgroundColor = Colors.grey[300];
---
+++ lib/pages/homepage.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_6/components/bottom_nav_bar.dart';
+import 'package:flutter_application_6/constant.dart';
+import 'package:flutter_application_6/pages/cartpage.dart';
+import 'package:flutter_application_6/pages/shoppage.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  void navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  // pages
+  final List<Widget> _pages = [
+    ShopPage(),
+    CartPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      bottomNavigationBar: MyBottomNavBar(
+        onTabChange: (index) => navigateBottomBar(index),
+      ),
+      body: _pages[_selectedIndex],
+    );
+  }
+}
